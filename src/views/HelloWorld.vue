1afbf0e2deb80cb382b297576ebd10658ee82d91
Michael <michael.alexander.romanov@gmail.com>
2019-06-11 09:21:46 -0600

    new commit

---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="Details d-flex justify-content-center">
+
+
+</div>
+
+</template>
+
+<script>
+    export default{
+        name: "",
+        prop: "", 
+        data(){
+            return {
+
+            }
+        }
+    }, 
+    mounted(){
+
+    },
+    computed: {
+
+    }, 
+    methods: {
+     addComment(){
+         let data = {
+
+         }
+     }, 
+     removeComment(id){
+
+     }, 
+     removePost(id){
+
+     }
+    }
+
+</script>
+
+
+
+
+<style
